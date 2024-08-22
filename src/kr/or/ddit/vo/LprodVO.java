834934682498a093359c897d9c31160609b63fbb
KJUKing <kyungjurhdqn@gmail.com>
2024-08-22 17:37:47 +0900

    240822 maven mybatis

---
+++ src/kr/or/ddit/vo/LprodVO.java
@@ -0,0 +1,46 @@
+package kr.or.ddit.vo;
+
+public class LprodVO {
+    private int lprod_id;
+    private int lprod_gu;
+    private String lprod_nm;
+
+    public LprodVO(int lprod_id, int lprod_gu, String lprod_nm) {
+        this.lprod_id = lprod_id;
+        this.lprod_gu = lprod_gu;
+        this.lprod_nm = lprod_nm;
+    }
+
+    public int getLprod_id() {
+        return lprod_id;
+    }
+
+    public void setLprod_id(int lprod_id) {
+        this.lprod_id = lprod_id;
+    }
+
+    public int getLprod_gu() {
+        return lprod_gu;
+    }
+
+    public void setLprod_gu(int lprod_gu) {
+        this.lprod_gu = lprod_gu;
+    }
+
+    public String getLprod_nm() {
+        return lprod_nm;
+    }
+
+    public void setLprod_nm(String lprod_nm) {
+        this.lprod_nm = lprod_nm;
+    }
+
+    @Override
+    public String toString() {
+        return "LprodVO{" +
+                "lprod_id=" + lprod_id +
+                ", lprod_gu=" + lprod_gu +
+                ", lprod_nm='" + lprod_nm + '\'' +
+                '}';
+    }
+}
